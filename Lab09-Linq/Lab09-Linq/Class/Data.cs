1d4a69d04fc84b27a4315a49c5478f7c8dc064af
Bryant Davis <bryant.davis@hotmail.com>
2020-07-26 19:31:11 -0700

    finished lab

---
+++ Lab09-Linq/Lab09-Linq/Class/Data.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab09_Linq.Class
+{
+    public class Data
+    {
+        public string Type { get; set; }
+        public List<Feature> Features { get; set; }
+    }
+
+    public class Feature
+    {
+        public string Type { get; set; }
+        public Geometry Geometry { get; set; }
+        public Properties Properties { get; set; }
+    }
+
+    public class Geometry
+    {
+        public string Type { get; set; }
+        public string[] Coordinates { get; set; }
+    }
+
+    public class Properties
+    {
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Address { get; set; }
+        public string Borough { get; set; }
+        public string Neighborhood { get; set; }
+        public string County { get; set; }
+    }
+}
